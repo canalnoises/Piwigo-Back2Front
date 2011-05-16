276bdb31515ebd00c07c62862cef88837f2adcec
ddtddt <ddtddt@piwigo.org>
2011-05-16 20:00:33 +0000

    [extensions] - Back2Front - add lv_LV (Latvian) thanks to Aivars Baldone
    git-svn-id: http://piwigo.org/svn/extensions/Back2Front@10908 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/lv_LV/plugin.lang.php
@@ -0,0 +1,21 @@
+<?php
+
+
+$lang['See back'] = 'Skatīt aizmugures daļu';
+$lang['See front'] = 'Skatīt priekšpusi';
+$lang['Modify backside information'] = 'Modificēt aizmugures daļas informāciju';
+
+$lang['This picture is a backside...'] = 'Šis attēls ir aizmugure...';
+$lang['...of the picture n°'] = '...attēlam n°';
+$lang['Backside management'] = 'Aizmugures daļas pārvalde';
+$lang['Move backside to private album'] = 'Pārvietot attēla aizmugures daļu uz privātu albūmu';
+$lang['This picture has a backside : '] = 'Šim attēlam ir aizmugure : ';
+
+$lang['Backside and frontside can\'t be the same picture'] = 'Aizmugure un priekšpuse nevar būt viens un tas pats attēls';
+$lang['This picture has already a backside : '] = 'Šim attēlam jau ir aizmugure : ';
+$lang['This picture id already a backside : '] = 'Šis attēls jau ir aizmugure : ';
+$lang['This picture is now the backside of the picture n° '] = 'Šis attēls tagad ir attēla n°  aizmugure ';
+$lang['Unknown id for frontside picture : '] = 'Nezināms priekšpuses attēla id : ';
+$lang['This picture is no longer a backside'] = '’Šis attēls vairs nav aizmugure';
+
+?>
