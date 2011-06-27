c6e65213c42f66671beefbd45b5664003dce004d
riopwg <riopwg@piwigo.org>
2011-06-27 09:22:54 +0000

    language IT for Back2Front
    git-svn-id: http://piwigo.org/svn/extensions/Back2Front@11519 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/it_IT/plugin.lang.php
@@ -0,0 +1,33 @@
+<?php
+
+$lang['See back'] = 'Vedere il retro';
+$lang['See front'] = 'Vedere il fronte';
+$lang['Modify backside information'] = 'Modificare le informazioni sul retro';
+
+$lang['Switch mode'] = 'Inervertire le foto al';
+$lang['Click'] = 'Clic';
+$lang['Mouseover'] = 'Sorvolo';
+$lang['Transition'] = 'Transizione';
+$lang['Fade'] = 'Fade';
+$lang['Link position'] = 'Pulzante posizione';
+$lang['Top'] = 'Alto';
+$lang['Bottom'] = 'Basso';
+$lang['Toolbar'] = 'Toolbar';
+$lang['Link name'] = 'Nome del pulzante';
+
+$lang['Seperate the two labels with the | symbol. Leave blank to use default translation.'] = 'Separare i due testi con il carattere |. Lasciare vuoto per usare la traduzione di default';
+
+$lang['This picture is a backside...'] = 'Questa foto è il retro...';
+$lang['...of the picture n°'] = '... della foto n°';
+$lang['Backside management'] = 'Gestione delle fronte-retro';
+$lang['Move backside to private album'] = 'Spostare il retro verso un\'album privato';
+$lang['This picture has a backside : '] = 'Questa foto a un retro : ';
+
+$lang['Backside and frontside can\'t be the same picture'] = 'Retro e fronte non possono essere la stessa foto';
+$lang['The picture n°%d has already a backside : %s'] = 'La foto n°%d ha già un retro';
+$lang['The picture n°%s is already a backside'] = 'La foto n°%s è già un retro';
+$lang['This picture is now the backside of the picture n°%s'] = 'Questa foto è ora il retro della foto n°%s';
+$lang['Unknown id %d for frontside picture'] = 'ID %d sconosciuto';
+$lang['This picture is no longer a backside'] = 'Quest foto non è più un retro';
+
+?>
