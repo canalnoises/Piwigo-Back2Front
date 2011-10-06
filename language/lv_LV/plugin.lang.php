f1cee922f124b3a8ed0ef0f9cae4d1721b06d090
mistic100 <mistic@piwigo.org>
2011-10-06 14:01:10 +0000

    a mistake in lv_LV file
    git-svn-id: http://piwigo.org/svn/extensions/Back2Front@12362 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/lv_LV/plugin.lang.php
+++ language/lv_LV/plugin.lang.php
@@ -26,7 +26,7 @@ $lang['This picture has a backside :'] = 'Šim attēlam ir aizmugure :';
 $lang['Backside and frontside can\'t be the same picture'] = 'Aizmugure un priekšpuse nevar būt viens un tas pats attēls';
 $lang['The picture n°%d has already a backside : %s'] = 'The picture n°%d has already a backside : %s';
 $lang['The picture n°%s is already a backside'] = 'The picture n°%s is already a backside';
-$lang['This picture is now the backside of the picture n° '] = 'Šis attēls tagad ir attēla n°  aizmugure ';
+$lang['This picture is now the backside of the picture n°%s'] = 'Šis attēls tagad ir attēla n°%s aizmugure';
 $lang['Unknown id %d for frontside picture'] = 'Unknown id %d for frontside picture';
 $lang['This picture is no longer a backside'] = '’Šis attēls vairs nav aizmugure';
 
