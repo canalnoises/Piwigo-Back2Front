7080164f4274afabbec4d25a4fbff303b5bfc0ca
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-02-29 07:38:55 +0000

    [Back2Front] Update it_IT, thanks to : Gdvpixel
    git-svn-id: http://piwigo.org/svn/extensions/Back2Front@13428 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/it_IT/plugin.lang.php
+++ language/it_IT/plugin.lang.php
@@ -9,25 +9,16 @@ $lang['Click'] = 'Clic';
 $lang['Mouseover'] = 'Sorvolo';
 $lang['Transition'] = 'Transizione';
 $lang['Fade'] = 'Fade';
-$lang['Link position'] = 'Pulzante posizione';
-$lang['Top'] = 'Alto';
-$lang['Bottom'] = 'Basso';
-$lang['Toolbar'] = 'Toolbar';
-$lang['Link name'] = 'Nome del pulzante';
-
-$lang['Seperate the two labels with the | symbol. Leave blank to use default translation.'] = 'Separare i due testi con il carattere |. Lasciare vuoto per usare la traduzione di default';
-
-$lang['This picture is a backside...'] = 'Questa foto è il retro...';
-$lang['...of the picture n°'] = '... della foto n°';
-$lang['Backside management'] = 'Gestione delle fronte-retro';
+$lang['Link position'] = 'Pulsante posizione';
 $lang['Move backside to private album'] = 'Spostare il retro verso un\'album privato';
-$lang['This picture has a backside :'] = 'Questa foto a un retro :';
+$lang['This picture has a backside :'] = 'Questa foto ha un retro :';
 
 $lang['Backside and frontside can\'t be the same picture'] = 'Retro e fronte non possono essere la stessa foto';
 $lang['The picture n°%d has already a backside : %s'] = 'La foto n°%d ha già un retro';
 $lang['The picture n°%s is already a backside'] = 'La foto n°%s è già un retro';
 $lang['This picture is now the backside of the picture n°%s'] = 'Questa foto è ora il retro della foto n°%s';
-$lang['Unknown id %d for frontside picture'] = 'ID %d sconosciuto';
+$lang['Unknown id %d for frontside picture'] = 'ID %d sconosciuto per la foto fronte';
 $lang['This picture is no longer a backside'] = 'Quest foto non è più un retro';
 
+$lang['Display a mark on thumbnails list'] = 'Mostra un segno sulla lista delle anteprime';
 ?>
