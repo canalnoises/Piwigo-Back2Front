a82de065bb7de7461f9ea25612a68190a2fed374
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-05 23:02:29 +0000

    [back2front] Add tr_TR, thanks to : hakanselcuk
    git-svn-id: http://piwigo.org/svn/extensions/Back2Front@21228 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/tr_TR/plugin.lang.php
@@ -0,0 +1,33 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Toolbar'] = 'Toolbar';
+$lang['Top'] = 'Üst';
+$lang['Transition'] = 'Geçiş';
+$lang['Bottom'] = 'Alt';
+$lang['Click'] = 'Tıkla';
+$lang['Link name'] = 'Buton adı';
+$lang['Link position'] = 'Buton yeri';
+$lang['Mouseover'] = 'Mouse üzerinde';
+$lang['See back'] = 'Geri';
+$lang['See front'] = 'İleri';
+?>
