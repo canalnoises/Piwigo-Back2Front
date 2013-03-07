c4335bea1c5ed596bcedae56dbe718f8f7f9fe2c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-07 18:52:35 +0000

    [back2front] Update en_UK, thanks to : Kaare
    git-svn-id: http://piwigo.org/svn/extensions/Back2Front@21335 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/en_UK/plugin.lang.php
+++ language/en_UK/plugin.lang.php
@@ -16,16 +16,16 @@ $lang['Toolbar'] = 'Toolbar';
 $lang['Link name'] = 'Button name';
 $lang['Display a mark on thumbnails list'] = 'Display a mark on thumbnails list';
 
-$lang['Seperate the two labels with the | symbol. Leave blank to use default translation.'] = 'Seperate the two labels with the | symbol. Leave blank to use default translation.';
+$lang['Seperate the two labels with the | symbol. Leave blank to use default translation.'] = 'Separate the two labels with the | symbol. Leave blank to use default translation.';
 
 $lang['This picture is a backside...'] = 'This picture is a backside...';
 $lang['...of the picture n°'] = '...of the picture n°';
 $lang['Backside management'] = 'Backside management';
 $lang['Move backside to private album'] = 'Move backside to private album';
-$lang['This picture has a backside :'] = 'This picture has a backside : ';
+$lang['This picture has a backside :'] = 'This picture has a backside: ';
 
 $lang['Backside and frontside can\'t be the same picture'] = 'Backside and frontside can\'t be the same picture';
-$lang['The picture n°%d has already a backside : %s'] = 'The picture n°%d has already a backside : %s';
+$lang['The picture n°%d has already a backside : %s'] = 'The picture n°%d has already a backside: %s';
 $lang['The picture n°%s is already a backside'] = 'The picture n°%s is already a backside';
 $lang['This picture is now the backside of the picture n°%s'] = 'This picture is now the backside of the picture n°%s';
 $lang['Unknown id %d for frontside picture'] = 'Unknown id %d for frontside picture';
