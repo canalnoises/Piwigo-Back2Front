289b7c378730463956c5fdadeb8ce26e39a9ec4f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-07 18:52:42 +0000

    [back2front] Add da_DK, thanks to : Kaare
    git-svn-id: http://piwigo.org/svn/extensions/Back2Front@21336 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/da_DK/plugin.lang.php
@@ -0,0 +1,49 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['This picture is no longer a backside'] = 'Billedet er ikke længere en bagside';
+$lang['This picture is now the backside of the picture n°%s'] = 'Billedet er nu bagsiden til billede nummer %s';
+$lang['Toolbar'] = 'Værktøjslinje';
+$lang['Top'] = 'Top';
+$lang['Transition'] = 'Glidende overgang';
+$lang['Unknown id %d for frontside picture'] = 'Ukendt id %d for forsidebilledet';
+$lang['...of the picture n°'] = '... af billedet med nummer';
+$lang['Backside and frontside can\'t be the same picture'] = 'Bagside og forside kan ikke være det samme billede';
+$lang['Backside management'] = 'Håndtering af bagside';
+$lang['Bottom'] = 'Bund';
+$lang['Click'] = 'Klik';
+$lang['Display a mark on thumbnails list'] = 'Vis et mærke på listen over miniaturebilleder';
+$lang['Fade'] = 'Fade';
+$lang['Link name'] = 'Knapnavn';
+$lang['Link position'] = 'Knapposition';
+$lang['Modify backside information'] = 'Modificer bagsideoplysninger';
+$lang['Mouseover'] = 'Mouseover';
+$lang['Move backside to private album'] = 'Flyt bagside til privat album';
+$lang['See back'] = 'Se bagside';
+$lang['See front'] = 'Se forside';
+$lang['Seperate the two labels with the | symbol. Leave blank to use default translation.'] = 'Adskil de to etiketter med |-tegnet. Hold feltet tomt for at benytte standardoversættelsen.';
+$lang['Switch mode'] = 'Skift tilstand';
+$lang['The picture n°%d has already a backside : %s'] = 'Billede nummer %d har allerede en bagside: %s';
+$lang['The picture n°%s is already a backside'] = 'Billede nummer %s er allerede en bagside';
+$lang['This picture has a backside :'] = 'Billedet har en bagside:';
+$lang['This picture is a backside...'] = 'Dette billede er en bagside...';
+?>
