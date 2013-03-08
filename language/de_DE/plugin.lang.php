48699d288be97168bce1858ae337079ccce1b8dd
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-08 19:24:52 +0000

    [back2front] Add de_DE, thanks to : Yogie
    git-svn-id: http://piwigo.org/svn/extensions/Back2Front@21386 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/de_DE/plugin.lang.php
@@ -0,0 +1,49 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Unknown id %d for frontside picture'] = 'Unbekannte ID %d für das Vorderseiten-Bild';
+$lang['Bottom'] = 'Fuß';
+$lang['Toolbar'] = 'Symbolleiste';
+$lang['Top'] = 'Kopf';
+$lang['Transition'] = 'Übergang';
+$lang['Seperate the two labels with the | symbol. Leave blank to use default translation.'] = 'Separiere die beiden Tabs mit dem | Zeichen. Lasse es leer um die Standard-Übersetzung zu benutzen.';
+$lang['Switch mode'] = 'Modus umschalten';
+$lang['The picture n°%d has already a backside : %s'] = 'Das Bild Nr. %d hat bereits eine Rückseite: %s';
+$lang['The picture n°%s is already a backside'] = 'Das Bild Nr. %s ist bereits eine Rückseite';
+$lang['This picture has a backside :'] = 'Dieses Bild hat eine Rückseite:';
+$lang['This picture is a backside...'] = 'Dieses Bild ist eine Rückseite...';
+$lang['This picture is no longer a backside'] = 'Dieses Bild ist nicht länger eine Rückseite';
+$lang['This picture is now the backside of the picture n°%s'] = 'Dieses Bild ist nun die Rückseite des Bildes Nr. %s';
+$lang['...of the picture n°'] = '... der Bildnummer';
+$lang['Backside and frontside can\'t be the same picture'] = 'Vorder- und Rückseite können nicht das gleiche Bild sein';
+$lang['Backside management'] = 'Rückseiten Einstellungen';
+$lang['Click'] = 'Klick';
+$lang['Display a mark on thumbnails list'] = 'Zeige ein Zeichen auf der Vorschaubild-Liste';
+$lang['Fade'] = 'Blenden';
+$lang['Link name'] = 'Schaltfläche Name';
+$lang['Link position'] = 'Schaltfläche Position';
+$lang['Modify backside information'] = 'Editiere Rückseiten Informationen';
+$lang['Mouseover'] = 'Mausover';
+$lang['Move backside to private album'] = 'Bewege Rückseite in privates Album';
+$lang['See back'] = 'Schau die Rückseite an';
+$lang['See front'] = 'Schau die Vorderseite an';
+?>
