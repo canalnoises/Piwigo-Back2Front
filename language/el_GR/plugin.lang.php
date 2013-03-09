49f4d7c6f8aaa174b9e7b30d0e7c5d8bc291635c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-09 10:41:10 +0000

    [back2front] Add el_GR, thanks to : bas_alba
    git-svn-id: http://piwigo.org/svn/extensions/Back2Front@21410 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/el_GR/plugin.lang.php
@@ -0,0 +1,49 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Bottom'] = 'Κάτω μέρος';
+$lang['Unknown id %d for frontside picture'] = 'Άγνωστη ταυτότητα %d για την εμπρός πλευρά της φωτογραφίας';
+$lang['Transition'] = 'Μετάβαση';
+$lang['Top'] = 'Επάνω μέρος';
+$lang['Toolbar'] = 'Γραμμή εργαλείων';
+$lang['This picture is now the backside of the picture n°%s'] = 'Αυτή η εικόνα είναι τώρα η πίσω πλευρά της εικόνας n°%s';
+$lang['This picture is no longer a backside'] = 'Αυτή η εικόνα είναι πλέον πίσω πλευρά';
+$lang['This picture is a backside...'] = 'Αυτή η εικόνα είναι μια πίσω πλευρά:';
+$lang['This picture has a backside :'] = 'Αυτή η εικόνα έχει μια πίσω πλευρά:';
+$lang['The picture n°%s is already a backside'] = 'Η εικόνα n°%d είναι ήδη πίσω μέρος';
+$lang['The picture n°%d has already a backside : %s'] = 'Η εικόνα n°%d έχει ήδη πίσω μέρος';
+$lang['Switch mode'] = 'Εναλλαγή λειτουργίας';
+$lang['Seperate the two labels with the | symbol. Leave blank to use default translation.'] = 'Διαχωρίστε τις δύο ετικέτες με το σύμβολο \'|\'. Αφήστε κενό για να χρησιμοποιήσετε την προεπιλεγμένη μετάφραση .';
+$lang['See front'] = 'Δείτε το εμπρός μέρος';
+$lang['See back'] = 'Δείτε το πίσω μέρος';
+$lang['Move backside to private album'] = 'Μετακίνηση Backside σε ιδιωτικό λεύκωμα';
+$lang['Mouseover'] = 'Mouseover';
+$lang['Modify backside information'] = 'Τροποποιήστε τις πληροφορίες του Backside ';
+$lang['Link position'] = 'Θέση κουμπιού';
+$lang['Link name'] = 'Όνομα κουμπιού';
+$lang['Fade'] = 'Ξεθώριασμα ';
+$lang['Display a mark on thumbnails list'] = 'Εμφάνιση ενός σήματος στη λίστα μικρογραφιών ';
+$lang['Click'] = 'Κλικ';
+$lang['Backside management'] = 'Διαχείριση Backside ';
+$lang['Backside and frontside can\'t be the same picture'] = 'Η Πίσω και η Εμπρός πλευρά δεν μπορεί να είναι η ίδια εικόνα';
+$lang['...of the picture n°'] = '...της εικόνας n°';
+?>
