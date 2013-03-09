ac4ab18a30b8cf0de439644495dac1bafa50b18a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-09 10:38:37 +0000

    [back2front] Update tr_TR, thanks to : LazBoy
    git-svn-id: http://piwigo.org/svn/extensions/Back2Front@21402 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/tr_TR/plugin.lang.php
+++ language/tr_TR/plugin.lang.php
@@ -20,7 +20,7 @@
 // | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
 // | USA.                                                                  |
 // +-----------------------------------------------------------------------+
-$lang['Toolbar'] = 'Toolbar';
+$lang['Toolbar'] = 'Araç çubuğu';
 $lang['Top'] = 'Üst';
 $lang['Transition'] = 'Geçiş';
 $lang['Bottom'] = 'Alt';
@@ -28,6 +28,22 @@ $lang['Click'] = 'Tıkla';
 $lang['Link name'] = 'Buton adı';
 $lang['Link position'] = 'Buton yeri';
 $lang['Mouseover'] = 'Mouse üzerinde';
-$lang['See back'] = 'Geri';
-$lang['See front'] = 'İleri';
+$lang['See back'] = 'Arka kısma bak';
+$lang['See front'] = 'Ön tarafına bak';
+$lang['This picture is no longer a backside'] = 'Bu fotoğraf artık arka kısım değil';
+$lang['This picture is now the backside of the picture n°%s'] = 'Bu fotoğraf şimdi n°%s fotoğrafının arka kısmıdır';
+$lang['Unknown id %d for frontside picture'] = 'Ön kısım fotoğrafı için bilinmeyen tanım %d';
+$lang['Seperate the two labels with the | symbol. Leave blank to use default translation.'] = 'İki etiketi | sembolü ile ayırın. Varsayılan çeviriyi kullanmak için boş bırakın.';
+$lang['Backside and frontside can\'t be the same picture'] = 'Ön ve arka kısımlar aynı fotoğraftan oluşamaz';
+$lang['...of the picture n°'] = '...fotoğraf n° \'nin';
+$lang['Display a mark on thumbnails list'] = 'Pul resmi listesinde bir işaret göster';
+$lang['The picture n°%d has already a backside : %s'] = 'n°%d fotoğrafı zaten bir arka kısma sahip: %s';
+$lang['The picture n°%s is already a backside'] = 'n°%s fotoğrafı zaten bir arka kısımdır';
+$lang['This picture is a backside...'] = 'Bu fotoğraf bir arka kısımdır...';
+$lang['This picture has a backside :'] = 'Bu fotoğraf bir arka kısma sahip:';
+$lang['Switch mode'] = 'Tarzı değiştir';
+$lang['Move backside to private album'] = 'Arka kısmı özel albüme taşı';
+$lang['Modify backside information'] = 'Arka kısım bilgisini değiştir';
+$lang['Fade'] = 'Karart';
+$lang['Backside management'] = 'Arka kısım yönetimi';
 ?>
