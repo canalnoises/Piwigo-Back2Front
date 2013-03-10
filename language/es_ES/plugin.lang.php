5ecb8353713c31a7b6b9b0364b1148725393fa80
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-10 08:54:06 +0000

    [back2front] Add es_ES, thanks to : jpr928
    git-svn-id: http://piwigo.org/svn/extensions/Back2Front@21428 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/es_ES/plugin.lang.php
@@ -0,0 +1,49 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['This picture is no longer a backside'] = 'Esta imagen ya no es una parte trasera';
+$lang['This picture is now the backside of the picture n°%s'] = 'Esta imagen es ahora la parte trasera de la imagen nº%s';
+$lang['Toolbar'] = 'Barra de herramientas';
+$lang['Top'] = 'Arriba';
+$lang['Transition'] = 'Transición';
+$lang['Unknown id %d for frontside picture'] = 'Identificación desconocida de la imagen frontal %d';
+$lang['...of the picture n°'] = '... de la imagen n °';
+$lang['Backside and frontside can\'t be the same picture'] = 'Parte trasera y frontal no pueden ser misma imagen';
+$lang['Backside management'] = 'Gestión de la parte trasera';
+$lang['Bottom'] = 'Parte inferior';
+$lang['Click'] = 'Clic';
+$lang['Display a mark on thumbnails list'] = 'Mostrar una marca en la lista de miniaturas';
+$lang['Fade'] = 'Fundido';
+$lang['Link name'] = 'Nombre del botón';
+$lang['Link position'] = 'Posición del botón';
+$lang['Modify backside information'] = 'Modificar las informaciones de la parte trasera';
+$lang['Mouseover'] = 'Encima del ratón';
+$lang['Move backside to private album'] = 'Retrocede para el álbum privado';
+$lang['See back'] = 'Ver atrás';
+$lang['See front'] = 'Ver delante';
+$lang['Seperate the two labels with the | symbol. Leave blank to use default translation.'] = 'Separe las dos etiquetas con el símbolo |. Dejar en blanco para utilizar la traducción por defecto.';
+$lang['Switch mode'] = 'Cambiar modo';
+$lang['The picture n°%d has already a backside : %s'] = 'La foto nº%d ya tiene una parte trasera:%s';
+$lang['The picture n°%s is already a backside'] = 'La foto nº%s es ya una parte trasera';
+$lang['This picture has a backside :'] = 'Esta foto tiene una parte trasera:';
+$lang['This picture is a backside...'] = 'Esta foto ya es una parte trasera:';
+?>
