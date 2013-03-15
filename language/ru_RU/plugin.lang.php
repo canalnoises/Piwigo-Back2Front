98336e92607d70eebe81cde2f1bd191596f147f6
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-15 16:07:38 +0000

    [back2front] Add ru_RU, thanks to : marchelly
    git-svn-id: http://piwigo.org/svn/extensions/Back2Front@21570 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ru_RU/plugin.lang.php
@@ -0,0 +1,49 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['...of the picture n°'] = '... изображения n°';
+$lang['Backside and frontside can\'t be the same picture'] = 'Обратная и лицевая стороны не могут быть одним и тем же изображением';
+$lang['Backside management'] = 'Управление обратной стороной';
+$lang['Bottom'] = 'Низ';
+$lang['Click'] = 'Клик';
+$lang['Display a mark on thumbnails list'] = 'Отображать отметку на списке миниатюр';
+$lang['Fade'] = 'Тень';
+$lang['Link name'] = 'Имя кнопки';
+$lang['Link position'] = 'Положение кнопки';
+$lang['Modify backside information'] = 'Изменить информацию об обратной строне';
+$lang['Mouseover'] = 'Наведение мыши';
+$lang['Move backside to private album'] = 'Переместить обратную сторону в приватный альбом';
+$lang['See back'] = 'Посмотреть обратную сторону';
+$lang['See front'] = 'Посмотреть лицевую сторону';
+$lang['Seperate the two labels with the | symbol. Leave blank to use default translation.'] = 'Разделяйте ярлыки символом |. Оставьте пустым для значения по умолчанию';
+$lang['Switch mode'] = 'Режим переключения';
+$lang['The picture n°%d has already a backside : %s'] = 'Изображение n°%d уже содержит обратную сторону %s';
+$lang['The picture n°%s is already a backside'] = 'Изображение n°%d уже является обратной стороной';
+$lang['This picture has a backside :'] = 'Изображение уже содержит обратную сторону:';
+$lang['This picture is a backside...'] = 'Изображение является обратной стороной...';
+$lang['This picture is no longer a backside'] = 'Изображение более не является обратной стороной';
+$lang['This picture is now the backside of the picture n°%s'] = 'Теперь изображение является обратной стороной n°%d';
+$lang['Toolbar'] = 'Панель инструмнтов';
+$lang['Top'] = 'Верх';
+$lang['Transition'] = 'Переход';
+$lang['Unknown id %d for frontside picture'] = 'Неизвестный id %d для изображения обратной стороны';
+?>
