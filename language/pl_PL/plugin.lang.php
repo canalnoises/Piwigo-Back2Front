32ce6988b471717ae4b6933c5898906bcc6f692d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-17 14:32:37 +0000

    [back2front] Update pl_PL, thanks to : K.S.
    git-svn-id: http://piwigo.org/svn/extensions/Back2Front@21643 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pl_PL/plugin.lang.php
@@ -0,0 +1,49 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Backside management'] = 'Zarządzanie drugim planem';
+$lang['Modify backside information'] = 'Modyfikuj informacje drugoplanowe';
+$lang['Move backside to private album'] = 'Przenieś drugi plan do albumu prywatnego';
+$lang['...of the picture n°'] = '... obrazu o identyfikatorze';
+$lang['Backside and frontside can\'t be the same picture'] = 'Obrazy pierwszoplanowy i drugoplanowy nie może być taki sam';
+$lang['See back'] = 'Zobacz tył';
+$lang['See front'] = 'Zobacz przód';
+$lang['The picture n°%d has already a backside : %s'] = 'Obraz o identyfikatorze %s posiada już obraz drugoplanowy: %s';
+$lang['The picture n°%s is already a backside'] = 'Obraz o identyfikatorze %s już jest drugoplanowy';
+$lang['This picture has a backside :'] = 'Ten obraz posiada obraz drugoplanowy:';
+$lang['This picture is a backside...'] = 'Ten obraz jest drugoplanowy...';
+$lang['This picture is no longer a backside'] = 'Ten obraz nie jest już drugoplanowy';
+$lang['This picture is now the backside of the picture n°%s'] = 'Ten obraz jest teraz drugoplanowy dla obrazu nr %s';
+$lang['Unknown id %d for frontside picture'] = 'Nieznany identyfikator %d obrazu pierwszoplanowego';
+$lang['Seperate the two labels with the | symbol. Leave blank to use default translation.'] = 'Rozdziel dwie etykiety symbolem |. Zostaw puste by użyć tłumaczenia domyślnego.';
+$lang['Click'] = 'Kliknięcie';
+$lang['Mouseover'] = 'Najechanie myszką';
+$lang['Switch mode'] = 'Tryb przełączania';
+$lang['Display a mark on thumbnails list'] = 'Wyświetl znak na liście miniatur';
+$lang['Fade'] = 'Łagodne';
+$lang['Transition'] = 'Przejście';
+$lang['Bottom'] = 'Na dole';
+$lang['Toolbar'] = 'Na pasku narzędziowym';
+$lang['Top'] = 'U góry';
+$lang['Link name'] = 'Nazwa przycisku';
+$lang['Link position'] = 'Pozycja przycisku';
+?>
