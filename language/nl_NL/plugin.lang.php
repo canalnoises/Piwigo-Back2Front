100d8275789bbc25353bf17fd9c6e7357e60e18a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-20 12:42:54 +0000

    [back2front] Add nl_NL, thanks to : yjb
    git-svn-id: http://piwigo.org/svn/extensions/Back2Front@21744 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/nl_NL/plugin.lang.php
@@ -0,0 +1,41 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['This picture is no longer a backside'] = 'Deze foto is geen achterkant meer';
+$lang['Top'] = 'Bovenzijde';
+$lang['...of the picture n°'] = '.... van foto nr.';
+$lang['Backside and frontside can\'t be the same picture'] = 'De achterkant en de voorkant kunnen niet hetzelfde zijn.';
+$lang['Backside management'] = 'Beheer van de achterkant';
+$lang['Bottom'] = 'Onder';
+$lang['Click'] = 'Klik';
+$lang['Fade'] = 'Vervagen';
+$lang['Link name'] = 'Naam van de knop';
+$lang['Link position'] = 'Positie van de knop';
+$lang['Modify backside information'] = 'Verander de achterkant informatie ';
+$lang['Move backside to private album'] = 'Verplaats de achterkant naar een privé album';
+$lang['See back'] = 'Zie achterkant';
+$lang['See front'] = 'Zie voorkant';
+$lang['The picture n°%d has already a backside : %s'] = 'Foto nr. %d heeft al een achterkant: %s';
+$lang['The picture n°%s is already a backside'] = 'Foto nr. %s is al een achterkant';
+$lang['This picture has a backside :'] = 'Deze foto heeft een achterkant:';
+$lang['This picture is a backside...'] = 'Deze foto is een achterkant....';
+?>
