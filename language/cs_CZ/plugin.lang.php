4b7ae5c99217a6aa46eb348e018ca8348b91632e
ddtddt <ddtddt@piwigo.org>
2013-03-25 06:07:33 +0000

    [extensions] - Back2Front  -Add cs_CZ - Česky – Czech - Thanks to webprostor
    git-svn-id: http://piwigo.org/svn/extensions/Back2Front@21824 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/cs_CZ/plugin.lang.php
@@ -0,0 +1,34 @@
+<?php
+
+$lang['See back'] = 'Ukaž zadní náhled';
+$lang['See front'] = 'Ukaž přední náhled';
+$lang['Modify backside information'] = 'Úprava info o zadní straně';
+
+$lang['Switch mode'] = 'Přepnout zobrazení';
+$lang['Click'] = 'Klik';
+$lang['Mouseover'] = 'Najetím kurzoru';
+$lang['Transition'] = 'Přechodem';
+$lang['Fade'] = 'Slábnutím';
+$lang['Link position'] = 'Pozice tlačítka';
+$lang['Top'] = 'Nahoře';
+$lang['Bottom'] = 'Dole';
+$lang['Toolbar'] = 'Lišta';
+$lang['Link name'] = 'Název tlačítka';
+$lang['Display a mark on thumbnails list'] = 'Zobrazit značku na seznamu náhledů';
+
+$lang['Seperate the two labels with the | symbol. Leave blank to use default translation.'] = 'Odděluje dva popisky pomocí | symbolu. Ponechte pole prázdnépro použití výchozího překladu.';
+
+$lang['This picture is a backside...'] = 'Tento náhled fotky je zadní strana...';
+$lang['...of the picture n°'] = '...fotky n°';
+$lang['Backside management'] = 'Správa zadního náhledu';
+$lang['Move backside to private album'] = 'Přesunout zadní náhled do privatního alba';
+$lang['This picture has a backside :'] = 'Tato fotka má zadní stranu: ';
+
+$lang['Backside and frontside can\'t be the same picture'] = 'Zadní a přední strana fotky nemůže být stejný obrázek';
+$lang['The picture n°%d has already a backside : %s'] = 'Fotografie n°%d už má druhý náhled: %s';
+$lang['The picture n°%s is already a backside'] = 'Fotografie n°%s je také zadním náhledem';
+$lang['This picture is now the backside of the picture n°%s'] = 'Tato fotka je nyní zadním náhledem fotky n°%s';
+$lang['Unknown id %d for frontside picture'] = 'Neznámé id %d pro přední náhled fotky';
+$lang['This picture is no longer a backside'] = 'Tento obrázek není dlouho zadním náhledem';
+
+?>
