93db917cb92abebd50fbda5cbe13c417d2382e7e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-27 15:28:16 +0000

    [back2front] Add hu_HU, thanks to : miky
    git-svn-id: http://piwigo.org/svn/extensions/Back2Front@21867 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/hu_HU/plugin.lang.php
@@ -0,0 +1,49 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Mouseover'] = 'Mouseover (egés föléhúzás)';
+$lang['...of the picture n°'] = '...az alábbi képnek: n°';
+$lang['Toolbar'] = 'Eszköztár';
+$lang['This picture is no longer a backside'] = 'Ez a kép mostantól nem hátoldal';
+$lang['This picture is now the backside of the picture n°%s'] = 'Ez a kép mostantól a hátoldala az alábbi képnek: n°%s';
+$lang['Top'] = 'Felső rész';
+$lang['Transition'] = 'Átmenet';
+$lang['Unknown id %d for frontside picture'] = 'Ismeretlen azonosító (%d) elülső képnek';
+$lang['Backside and frontside can\'t be the same picture'] = 'Az elülső oldal és a hátoldal nem lehet ugyanaz a kép';
+$lang['Backside management'] = 'Hátoldal kezelés';
+$lang['Bottom'] = 'Alsó rész';
+$lang['Click'] = 'Kattintás';
+$lang['Display a mark on thumbnails list'] = 'Jel megjelenítése a bélyegképek listában';
+$lang['Fade'] = 'Halványítás';
+$lang['Link name'] = 'Gomb neve';
+$lang['Link position'] = 'Gomb helye';
+$lang['Modify backside information'] = 'Hátoldali információ módosítása';
+$lang['Move backside to private album'] = 'Hátoldal mozgatása privát albumba';
+$lang['See back'] = 'Hátoldal megtekintése';
+$lang['See front'] = 'Elülső oldal megtekintése';
+$lang['Seperate the two labels with the | symbol. Leave blank to use default translation.'] = 'A két címkét válaszd el a | jellel. Hagy üresen az alapértelmezett fordításhoz';
+$lang['Switch mode'] = 'Mód váltás';
+$lang['The picture n°%d has already a backside : %s'] = 'A képnek (n°%d) már van hátoldala: %s';
+$lang['The picture n°%s is already a backside'] = 'A kép (n°%s) már egy hátoldal';
+$lang['This picture has a backside :'] = 'Ennek a képnek van egy hátoldala';
+$lang['This picture is a backside...'] = 'Ez a kép egy hátoldal';
+?>
