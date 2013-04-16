85680f5de286ffb8cdf2a6c987b840e62632d971
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-04-16 05:19:25 +0000

    [back2front] Add zh_CN, thanks to : dennisyan
    git-svn-id: http://piwigo.org/svn/extensions/Back2Front@22236 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_CN/plugin.lang.php
@@ -0,0 +1,49 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Seperate the two labels with the | symbol. Leave blank to use default translation.'] = '以 | 符号分隔两个标记。留空则使用默认转换。';
+$lang['...of the picture n°'] = '...of 图片 n°';
+$lang['This picture is no longer a backside'] = '本图片已不再是反面';
+$lang['This picture is now the backside of the picture n°%s'] = '本图片是图片 n°%s 的反面';
+$lang['Toolbar'] = '工具条';
+$lang['Top'] = '顶部';
+$lang['Transition'] = '转换';
+$lang['Unknown id %d for frontside picture'] = '未知的正面图片id %d';
+$lang['Backside and frontside can\'t be the same picture'] = '反面和正面不能是同一张图片';
+$lang['Backside management'] = '反面设置';
+$lang['Bottom'] = '底部';
+$lang['Click'] = '点击';
+$lang['Display a mark on thumbnails list'] = '在缩略图列表中显示一个标记';
+$lang['Fade'] = '渐隐';
+$lang['Link name'] = '按钮名称';
+$lang['Link position'] = '按钮位置';
+$lang['Modify backside information'] = '修改反面信息';
+$lang['Mouseover'] = '鼠标悬停';
+$lang['Move backside to private album'] = '将反面移至非公开相册';
+$lang['See back'] = '查看反面';
+$lang['See front'] = '查看正面';
+$lang['Switch mode'] = '切换模式';
+$lang['The picture n°%d has already a backside : %s'] = '图片 n°%d 已有反面： %s';
+$lang['The picture n°%s is already a backside'] = '图片 n°%s 已被设为反面';
+$lang['This picture has a backside :'] = '本图片有反面：';
+$lang['This picture is a backside...'] = '本图片为反面...';
+?>
