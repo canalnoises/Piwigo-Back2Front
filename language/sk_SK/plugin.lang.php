2762719dca8938a8b9189e241ddc2a46d309b2b4
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-06-19 15:02:42 +0000

    [back2front] Add sk_SK, thanks to : xpace
    git-svn-id: http://piwigo.org/svn/extensions/Back2Front@23366 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sk_SK/plugin.lang.php
@@ -0,0 +1,49 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Mouseover'] = 'Mouseover';
+$lang['...of the picture n°'] = '...obrázku n°';
+$lang['Seperate the two labels with the | symbol. Leave blank to use default translation.'] = 'Oddeľ tie dve menovky so symbolom |. Nechaj prázdne pre použitie východzieho prekladu.';
+$lang['Modify backside information'] = 'Upraviť informácie zadnej strany';
+$lang['Transition'] = 'Prechod';
+$lang['Unknown id %d for frontside picture'] = 'Neznáme id %d obrázku na prednej strane';
+$lang['Fade'] = 'Zoslabiť';
+$lang['Switch mode'] = 'Prepnutie módu';
+$lang['The picture n°%d has already a backside : %s'] = 'Obrázok n°%d už má zadnú stranu: %s';
+$lang['The picture n°%s is already a backside'] = 'Obrázok n°%s už je zadná strana';
+$lang['This picture has a backside :'] = 'Tento obrázok má zadnú stranu';
+$lang['This picture is a backside...'] = 'Tento obrázok je zadná strana';
+$lang['This picture is no longer a backside'] = 'Tento obrázok už nie je zadná strana';
+$lang['This picture is now the backside of the picture n°%s'] = 'Tento obrázok už je zadnou stranou obrázku n°%s';
+$lang['Toolbar'] = 'Lišta nástrojov';
+$lang['Top'] = 'Vrch';
+$lang['Backside and frontside can\'t be the same picture'] = 'Zadná a predná strana nemôžu byť rovnaký obrázok';
+$lang['Backside management'] = 'Správa zadnej strany';
+$lang['Bottom'] = 'Spodok';
+$lang['Click'] = 'Klik';
+$lang['Display a mark on thumbnails list'] = 'Zobraz značku na zozname miniatúr';
+$lang['Hide backside from albums'] = 'Schovať zadnú stranu z albumov';
+$lang['Link name'] = 'Názov tlačidla';
+$lang['Link position'] = 'Pozícia tlačidla';
+$lang['See back'] = 'Ukáž zadok';
+$lang['See front'] = 'Ukáž predok';
+?>
