21e22ea5d0bbb172440a777834199906fb05dff8
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-10-25 09:50:30 +0000

    [back2front] Update nb_NO, thanks to : paulen
    git-svn-id: http://piwigo.org/svn/extensions/Back2Front@30302 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/nb_NO/plugin.lang.php
@@ -0,0 +1,48 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Toolbar'] = 'Verktøylinje';
+$lang['Top'] = 'Topp';
+$lang['Transition'] = 'Overgang';
+$lang['Unknown id %d for frontside picture'] = 'Ukjent id %d til forside bilde';
+$lang['This picture is now the backside of the picture n°%s'] = 'Dette bilde er nå baksiden til bilde n°%s';
+$lang['This picture is no longer a backside'] = 'Dette bilde er ikke lenger en bakside';
+$lang['The picture n°%d has already a backside : %s'] = 'Bilde n°%d har allerede en bakside: %s';
+$lang['...of the picture n°'] = '...av bildet n°';
+$lang['Backside and frontside can\'t be the same picture'] = 'Baksiden og forsiden kan ikke ha samme bilde';
+$lang['Backside management'] = 'Bakside behandling';
+$lang['Bottom'] = 'Knapp';
+$lang['Click'] = 'Klikk';
+$lang['Display a mark on thumbnails list'] = 'Vise et merke på miniatyrbilde listen';
+$lang['Fade'] = 'Ton';
+$lang['Hide backside from albums'] = 'Skjule baksiden fra album';
+$lang['Link name'] = 'Navn på knapp';
+$lang['Link position'] = 'Knapp possisjon';
+$lang['Modify backside information'] = 'Endre bakside informasjon';
+$lang['Mouseover'] = 'Mus over';
+$lang['See back'] = 'Se baksiden';
+$lang['See front'] = 'Se forsiden';
+$lang['Seperate the two labels with the | symbol. Leave blank to use default translation.'] = 'Skill de to etiketter med | symbol. La stå tomt for å bruke standard oversettelse.';
+$lang['Switch mode'] = 'Bytt modus';
+$lang['The picture n°%s is already a backside'] = 'Bilde n°%s er allerede en bakside';
+$lang['This picture has a backside :'] = 'Dette bilde har en bakside';
+$lang['This picture is a backside...'] = 'Dette bilde er en bakside';
