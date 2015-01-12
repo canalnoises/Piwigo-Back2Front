4e9b10dc6b9edf961ee8a31e06c764043fcf3f16
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-01-12 20:21:28 +0000

    Update uk_UA, thanks to m_alien
    git-svn-id: http://piwigo.org/svn/extensions/Back2Front@30889 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/uk_UA/plugin.lang.php
@@ -0,0 +1,48 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Click'] = 'Клік';
+$lang['Seperate the two labels with the | symbol. Leave blank to use default translation.'] = 'Розділяйте два заголовки таким | символом. Залиште пустим для використання перекладу за замовчанням.';
+$lang['Toolbar'] = 'Панель інструментів';
+$lang['Fade'] = 'Блякнути';
+$lang['Display a mark on thumbnails list'] = 'Показати позначку на переліку мініатюр';
+$lang['Unknown id %d for frontside picture'] = 'Невідоме id %d для зображення передньої сторінки';
+$lang['Hide backside from albums'] = 'Заховати зворотні сторінки від альбомів';
+$lang['This picture is now the backside of the picture n°%s'] = 'Тепер це зображення стало зворотньою сторінкою для зображення n°%s';
+$lang['This picture is no longer a backside'] = 'Це зображення вже не зворотня сторінка';
+$lang['This picture is a backside...'] = 'Це зображення звороньої сторони...';
+$lang['The picture n°%s is already a backside'] = 'Зображення n°%s вже є зворотньою стороною';
+$lang['The picture n°%d has already a backside : %s'] = 'Зображення n°%d вже має зворотню сторінку: %s';
+$lang['Mouseover'] = 'Наведення миші';
+$lang['Switch mode'] = 'Режим змінення';
+$lang['Transition'] = 'Перехід';
+$lang['Top'] = 'Верх';
+$lang['This picture has a backside :'] = 'Це зображення має зворотнью сторінку:';
+$lang['See front'] = 'Дивитись переднью сторону';
+$lang['See back'] = 'Дивитись зворотну сторону';
+$lang['Modify backside information'] = 'Змінити інформацію зворотньої сторінки';
+$lang['Link position'] = 'Розташування кнопки';
+$lang['Link name'] = 'Назва кнопки';
+$lang['Bottom'] = 'Низ';
+$lang['Backside and frontside can\'t be the same picture'] = 'Передня та зворотня сторінка не повинна бути однаковим зображенням';
+$lang['Backside management'] = 'Управління зворотньою сторінкою';
+$lang['...of the picture n°'] = '...зображення n°';
