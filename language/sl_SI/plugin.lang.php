d87474ffd56e79c809e85e84aa00732efa02acbc
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-12-24 14:46:49 +0100

    Update sl_SI, thanks to maxima

---
+++ language/sl_SI/plugin.lang.php
@@ -0,0 +1,33 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Unknown id %d for frontside picture'] = 'Neznan id %d za sprednjo sliko';
+$lang['This picture is now the backside of the picture n°%s'] = 'Ta slika je zdaj zadnja stran slike št°%s';
+$lang['This picture is no longer a backside'] = 'Ta slika ni več zadnja stran';
+$lang['This picture is a backside...'] = 'Ta slika je zadnja stran...';
+$lang['Transition'] = 'Prehod';
+$lang['Toolbar'] = 'Orodna vrstica';
+$lang['Top'] = 'Vrh';
+$lang['Switch mode'] = 'Preklopni način';
+$lang['The picture n°%d has already a backside : %s'] = 'Ta slika št°%d ima že zadnjo stran: %s';
+$lang['The picture n°%s is already a backside'] = 'Ta slika št°%s je že zadnja stran';
+$lang['This picture has a backside :'] = 'Ta slika ima zadnjo stran: ';
+$lang['Mouseover'] = 'Pomik miške';
+$lang['See back'] = 'Poglej nazaj';
+$lang['See front'] = 'Poglej naprej';
+$lang['Seperate the two labels with the | symbol. Leave blank to use default translation.'] = 'Obe nalepki ločite s | simbolom. Če želite uporabiti privzeti prevod, pustite prazno.';
+$lang['...of the picture n°'] = '... slika št°';
+$lang['Backside and frontside can\'t be the same picture'] = 'Zadnja in sprednja stran ne moreta biti enaki sliki';
+$lang['Backside management'] = 'Upravljanje zadaj';
+$lang['Bottom'] = 'Dno';
+$lang['Click'] = 'Klik';
+$lang['Display a mark on thumbnails list'] = 'Prikaži oznako na seznamu sličic';
+$lang['Fade'] = 'Zbledi';
+$lang['Hide backside from albums'] = 'Skrij zadnjo stran iz albumov';
+$lang['Link name'] = 'Ime gumba';
+$lang['Link position'] = 'Položaj gumba';
+$lang['Modify backside information'] = 'Spremenite podatke na zadnji strani';
