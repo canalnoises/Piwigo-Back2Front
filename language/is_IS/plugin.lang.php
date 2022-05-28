f6f72335c7d16087a6e7b4ef35be51cdd406aba3
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-05-28 09:28:13 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,33 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Mouseover'] = 'Mús yfir';
+$lang['See back'] = 'Sjá bakhlið';
+$lang['See front'] = 'Sjá framhlið';
+$lang['Seperate the two labels with the | symbol. Leave blank to use default translation.'] = 'Aðskildu merkimiðana tvo með | tákni. Skildu eftir autt til að nota sjálfgefna þýðingu.';
+$lang['Switch mode'] = 'Skiptu um ham';
+$lang['The picture n°%d has already a backside : %s'] = 'Myndin n°%d hefur þegar bakhlið: %s';
+$lang['The picture n°%s is already a backside'] = 'Myndin n°%s er þegar bakhlið';
+$lang['This picture has a backside :'] = 'Þessi mynd er með bakhlið:';
+$lang['This picture is a backside...'] = 'Þessi mynd er bakhlið...';
+$lang['This picture is no longer a backside'] = 'Þessi mynd er ekki lengur bakhlið';
+$lang['This picture is now the backside of the picture n°%s'] = 'Þessi mynd er nú bakhlið myndarinnar n°%s';
+$lang['Toolbar'] = 'Tækjastikan';
+$lang['Top'] = 'Efst';
+$lang['Transition'] = 'Umskipti';
+$lang['Unknown id %d for frontside picture'] = 'Óþekkt auðkenni %d fyrir mynd að framan';
+$lang['...of the picture n°'] = '...af myndinni n°';
+$lang['Backside and frontside can\'t be the same picture'] = 'Bakhlið og framhlið geta ekki verið sama myndin';
+$lang['Backside management'] = 'Bakhliðarstjórnun';
+$lang['Bottom'] = 'Neðst';
+$lang['Click'] = 'Smella';
+$lang['Display a mark on thumbnails list'] = 'Birta merki á lista yfir smámyndir';
+$lang['Fade'] = 'Deyfa';
+$lang['Hide backside from albums'] = 'Fela bakhliðina á albúmum';
+$lang['Link name'] = 'Nafn hnapps';
+$lang['Link position'] = 'Staða hnapps';
+$lang['Modify backside information'] = 'Breyttu upplýsingum um bakhliðina';
