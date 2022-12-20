e9aed9160ee98c81305709f56b71baf58f0d23ab
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-12-20 08:10:57 +0100

    Update sv_SE, thanks to PBobbenB

---
+++ language/sv_SE/plugin.lang.php
@@ -0,0 +1,33 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Bottom'] = 'Nederst';
+$lang['This picture is no longer a backside'] = 'Denna bild är inte längre en baksida';
+$lang['This picture is now the backside of the picture n°%s'] = 'Denna bild är nu en baksida av bilden n°%s';
+$lang['Toolbar'] = 'Verktygsrad';
+$lang['Top'] = 'Överst';
+$lang['Transition'] = 'Övergång';
+$lang['Unknown id %d for frontside picture'] = 'Okänt id %d för framsidebild';
+$lang['The picture n°%s is already a backside'] = 'Bilden n°%s är redan en baksida';
+$lang['...of the picture n°'] = '...av bilden n°';
+$lang['Backside and frontside can\'t be the same picture'] = 'Baksida och framsida kan inte vara samma bild';
+$lang['Backside management'] = 'Baksidehantering';
+$lang['Click'] = 'Klicka';
+$lang['Display a mark on thumbnails list'] = 'Visa en markering på miniatyrbildslistan';
+$lang['Fade'] = 'Tona ut';
+$lang['Hide backside from albums'] = 'Göm baksida från album';
+$lang['Link name'] = 'Knappens namn';
+$lang['Link position'] = 'Knappens läge';
+$lang['Modify backside information'] = 'Ändra baksidans information';
+$lang['Mouseover'] = 'Muspekaren över';
+$lang['See back'] = 'Se baksida';
+$lang['See front'] = 'Se framsida';
+$lang['Seperate the two labels with the | symbol. Leave blank to use default translation.'] = 'Separera de två etiketterna med | symbolen. Lämna tomt om du vill använda standardöversättning.';
+$lang['Switch mode'] = 'Växla läge';
+$lang['The picture n°%d has already a backside : %s'] = 'Bilden n°%d har redan en baksida: %s';
+$lang['This picture has a backside :'] = 'Denna bild har en baksida:';
+$lang['This picture is a backside...'] = 'Denna bild är en baksida...';
